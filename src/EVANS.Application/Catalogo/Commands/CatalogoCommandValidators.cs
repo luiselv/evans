afe3a3302b675f24f545779caf6f52d335162769
luiselv <luiselv@outlook.com>
2026-05-27 23:18:56 -0500

    feat(catalogo): validate application commands

---
+++ src/EVANS.Application/Catalogo/Commands/CatalogoCommandValidators.cs
@@ -0,0 +1,198 @@
+using EVANS.Domain.Shared;
+using FluentValidation;
+
+namespace EVANS.Application.Catalogo.Commands;
+
+public sealed class CreateClienteCommandValidator : AbstractValidator<CreateClienteCommand>
+{
+    public CreateClienteCommandValidator()
+    {
+        RuleFor(c => c.RazonSocial).NotEmpty();
+        RuleFor(c => c.TipoIdCodigo).GreaterThan(0);
+        RuleFor(c => c.LongitudRequerida).Must(length => length is 8 or 11);
+        RuleFor(c => c.NroIdentificacion)
+            .Cascade(CascadeMode.Stop)
+            .NotEmpty()
+            .Must((command, value) => value.Length == command.LongitudRequerida)
+            .WithMessage("NroIdentificacion length must match LongitudRequerida.")
+            .Must((command, value) => command.LongitudRequerida != 11 || Ruc.TryCreate(value, out _))
+            .WithMessage("RUC must be 11 numeric characters.");
+        RuleFor(c => c.Direcciones).NotEmpty();
+    }
+}
+
+public sealed class UpdateClienteCommandValidator : AbstractValidator<UpdateClienteCommand>
+{
+    public UpdateClienteCommandValidator()
+    {
+        RuleFor(c => c.Codigo).GreaterThan(0);
+        RuleFor(c => c.RazonSocial).NotEmpty();
+        RuleFor(c => c.TipoIdCodigo).GreaterThan(0);
+        RuleFor(c => c.LongitudRequerida).Must(length => length is 8 or 11);
+        RuleFor(c => c.NroIdentificacion)
+            .Cascade(CascadeMode.Stop)
+            .NotEmpty()
+            .Must((command, value) => value.Length == command.LongitudRequerida)
+            .WithMessage("NroIdentificacion length must match LongitudRequerida.")
+            .Must((command, value) => command.LongitudRequerida != 11 || Ruc.TryCreate(value, out _))
+            .WithMessage("RUC must be 11 numeric characters.");
+        RuleFor(c => c.Direcciones).NotEmpty();
+    }
+}
+
+public sealed class CreateEmpresaCommandValidator : AbstractValidator<CreateEmpresaCommand>
+{
+    public CreateEmpresaCommandValidator()
+    {
+        RuleFor(c => c.RazonSocial).NotEmpty();
+        RuleFor(c => c.Ruc).Must(value => Ruc.TryCreate(value, out _))
+            .WithMessage("RUC must be 11 numeric characters.");
+    }
+}
+
+public sealed class UpdateEmpresaCommandValidator : AbstractValidator<UpdateEmpresaCommand>
+{
+    public UpdateEmpresaCommandValidator()
+    {
+        RuleFor(c => c.Codigo).GreaterThan(0);
+        RuleFor(c => c.RazonSocial).NotEmpty();
+        RuleFor(c => c.Ruc).Must(value => Ruc.TryCreate(value, out _))
+            .WithMessage("RUC must be 11 numeric characters.");
+    }
+}
+
+public sealed class DeactivateEmpresaCommandValidator : AbstractValidator<DeactivateEmpresaCommand>
+{
+    public DeactivateEmpresaCommandValidator() => RuleFor(c => c.Codigo).GreaterThan(0);
+}
+
+public sealed class CreateVehiculoCommandValidator : AbstractValidator<CreateVehiculoCommand>
+{
+    public CreateVehiculoCommandValidator()
+    {
+        RuleFor(c => c.Placa).NotEmpty();
+        RuleFor(c => c.ConfiguracionVehicular).NotEmpty();
+        RuleFor(c => c.EmpresaCodigo).GreaterThan(0);
+    }
+}
+
+public sealed class UpdateVehiculoCommandValidator : AbstractValidator<UpdateVehiculoCommand>
+{
+    public UpdateVehiculoCommandValidator()
+    {
+        RuleFor(c => c.Codigo).GreaterThan(0);
+        RuleFor(c => c.Placa).NotEmpty();
+        RuleFor(c => c.ConfiguracionVehicular).NotEmpty();
+        RuleFor(c => c.EmpresaCodigo).GreaterThan(0);
+    }
+}
+
+public sealed class DeactivateVehiculoCommandValidator : AbstractValidator<DeactivateVehiculoCommand>
+{
+    public DeactivateVehiculoCommandValidator() => RuleFor(c => c.Codigo).GreaterThan(0);
+}
+
+public sealed class CreateCarretaCommandValidator : AbstractValidator<CreateCarretaCommand>
+{
+    public CreateCarretaCommandValidator()
+    {
+        RuleFor(c => c.Placa).NotEmpty();
+        RuleFor(c => c.EmpresaCodigo).GreaterThan(0);
+    }
+}
+
+public sealed class UpdateCarretaCommandValidator : AbstractValidator<UpdateCarretaCommand>
+{
+    public UpdateCarretaCommandValidator()
+    {
+        RuleFor(c => c.Codigo).GreaterThan(0);
+        RuleFor(c => c.Placa).NotEmpty();
+        RuleFor(c => c.EmpresaCodigo).GreaterThan(0);
+    }
+}
+
+public sealed class DeactivateCarretaCommandValidator : AbstractValidator<DeactivateCarretaCommand>
+{
+    public DeactivateCarretaCommandValidator() => RuleFor(c => c.Codigo).GreaterThan(0);
+}
+
+public sealed class CreateChoferCommandValidator : AbstractValidator<CreateChoferCommand>
+{
+    public CreateChoferCommandValidator()
+    {
+        RuleFor(c => c.NombreCompleto).NotEmpty();
+        RuleFor(c => c.Licencia).NotEmpty();
+        RuleFor(c => c.EmpresaCodigo).GreaterThan(0);
+    }
+}
+
+public sealed class UpdateChoferCommandValidator : AbstractValidator<UpdateChoferCommand>
+{
+    public UpdateChoferCommandValidator()
+    {
+        RuleFor(c => c.Codigo).GreaterThan(0);
+        RuleFor(c => c.NombreCompleto).NotEmpty();
+        RuleFor(c => c.Licencia).NotEmpty();
+        RuleFor(c => c.EmpresaCodigo).GreaterThan(0);
+    }
+}
+
+public sealed class DeactivateChoferCommandValidator : AbstractValidator<DeactivateChoferCommand>
+{
+    public DeactivateChoferCommandValidator() => RuleFor(c => c.Codigo).GreaterThan(0);
+}
+
+public sealed class CreateDestinoCommandValidator : AbstractValidator<CreateDestinoCommand>
+{
+    public CreateDestinoCommandValidator()
+    {
+        RuleFor(c => c.Descripcion).NotEmpty();
+        RuleFor(c => c.DistanciaVirtual).GreaterThanOrEqualTo(0);
+    }
+}
+
+public sealed class UpdateDestinoCommandValidator : AbstractValidator<UpdateDestinoCommand>
+{
+    public UpdateDestinoCommandValidator()
+    {
+        RuleFor(c => c.Codigo).GreaterThan(0);
+        RuleFor(c => c.Descripcion).NotEmpty();
+        RuleFor(c => c.DistanciaVirtual).GreaterThanOrEqualTo(0);
+    }
+}
+
+public sealed class DeactivateDestinoCommandValidator : AbstractValidator<DeactivateDestinoCommand>
+{
+    public DeactivateDestinoCommandValidator() => RuleFor(c => c.Codigo).GreaterThan(0);
+}
+
+public sealed class CreateEstadoCommandValidator : AbstractValidator<CreateEstadoCommand>
+{
+    public CreateEstadoCommandValidator() => RuleFor(c => c.Descripcion).NotEmpty();
+}
+
+public sealed class UpdateEstadoCommandValidator : AbstractValidator<UpdateEstadoCommand>
+{
+    public UpdateEstadoCommandValidator()
+    {
+        RuleFor(c => c.Codigo).GreaterThan(0);
+        RuleFor(c => c.Descripcion).NotEmpty();
+    }
+}
+
+public sealed class CreateTipoIdentificacionCommandValidator : AbstractValidator<CreateTipoIdentificacionCommand>
+{
+    public CreateTipoIdentificacionCommandValidator()
+    {
+        RuleFor(c => c.Descripcion).NotEmpty();
+    }
+}
+
+public sealed class UpdateTipoIdentificacionCommandValidator : AbstractValidator<UpdateTipoIdentificacionCommand>
+{
+    public UpdateTipoIdentificacionCommandValidator()
+    {
+        RuleFor(c => c.Codigo).Must(codigo => codigo is 1 or 2);
+        RuleFor(c => c.Descripcion).NotEmpty();
+    }
+}
---
+++ tests/EVANS.Application.Tests/Catalogo/CatalogoCommandValidatorTests.cs
@@ -0,0 +1,129 @@
+using EVANS.Application.Catalogo.Commands;
+using EVANS.Application.Catalogo.DTOs;
+using FluentValidation.TestHelper;
+
+namespace EVANS.Application.Tests.Catalogo;
+
+public class CatalogoCommandValidatorTests
+{
+    [Fact]
+    public void CreateClienteValidator_WithoutDirecciones_HasValidationError()
+    {
+        var validator = new CreateClienteCommandValidator();
+        var command = new CreateClienteCommand("ACME", 1, "20123456789", 11, null, null, []);
+
+        var result = validator.TestValidate(command);
+
+        result.ShouldHaveValidationErrorFor(c => c.Direcciones);
+    }
+
+    [Fact]
+    public void CreateClienteValidator_ValidDniWithLeadingZero_HasNoValidationError()
+    {
+        var validator = new CreateClienteCommandValidator();
+        var command = new CreateClienteCommand(
+            "ACME",
+            2,
+            "07654321",
+            8,
+            null,
+            null,
+            [new DireccionDto("Av Lima", "Lima", "Lima")]);
+
+        var result = validator.TestValidate(command);
+
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+
+    [Fact]
+    public void CreateEmpresaValidator_InvalidRuc_HasValidationError()
+    {
+        var validator = new CreateEmpresaCommandValidator();
+        var command = new CreateEmpresaCommand("TRANSPORT SA", null, null, "123", false);
+
+        var result = validator.TestValidate(command);
+
+        result.ShouldHaveValidationErrorFor(c => c.Ruc);
+    }
+
+    [Fact]
+    public void CreateClienteValidator_NullNroIdentificacion_ReturnsValidationError()
+    {
+        var validator = new CreateClienteCommandValidator();
+        var command = new CreateClienteCommand(
+            "ACME",
+            2,
+            null!,
+            8,
+            null,
+            null,
+            [new DireccionDto("Av Lima", "Lima", "Lima")]);
+
+        var result = validator.TestValidate(command);
+
+        result.ShouldHaveValidationErrorFor(c => c.NroIdentificacion);
+    }
+
+    [Fact]
+    public void UpdateClienteValidator_NullNroIdentificacion_ReturnsValidationError()
+    {
+        var validator = new UpdateClienteCommandValidator();
+        var command = new UpdateClienteCommand(
+            1,
+            "ACME",
+            2,
+            null!,
+            8,
+            null,
+            null,
+            [new DireccionDto("Av Lima", "Lima", "Lima")]);
+
+        var result = validator.TestValidate(command);
+
+        result.ShouldHaveValidationErrorFor(c => c.NroIdentificacion);
+    }
+
+    [Fact]
+    public void CreateTipoIdentificacionValidator_WithoutDescripcion_HasValidationError()
+    {
+        var validator = new CreateTipoIdentificacionCommandValidator();
+        var command = new CreateTipoIdentificacionCommand("");
+
+        var result = validator.TestValidate(command);
+
+        result.ShouldHaveValidationErrorFor(c => c.Descripcion);
+    }
+
+    [Fact]
+    public void CreateVehiculoValidator_WithoutPlaca_HasValidationError()
+    {
+        var validator = new CreateVehiculoCommandValidator();
+        var command = new CreateVehiculoCommand("Volvo", "", "C2", null, 10);
+
+        var result = validator.TestValidate(command);
+
+        result.ShouldHaveValidationErrorFor(c => c.Placa);
+    }
+
+    [Fact]
+    public void DeactivateEmpresaValidator_CodigoZero_HasValidationError()
+    {
+        var validator = new DeactivateEmpresaCommandValidator();
+
+        var result = validator.TestValidate(new DeactivateEmpresaCommand(0));
+
+        result.ShouldHaveValidationErrorFor(c => c.Codigo);
+    }
+
+    [Theory]
+    [InlineData("EVANS.Application.Catalogo.Commands.DeactivateClienteCommand")]
+    [InlineData("EVANS.Application.Catalogo.Commands.DeactivateEstadoCommand")]
+    [InlineData("EVANS.Application.Catalogo.Commands.DeactivateTipoIdentificacionCommand")]
+    [InlineData("EVANS.Application.Catalogo.Commands.CreateAgenciaCommand")]
+    [InlineData("EVANS.Application.Catalogo.Commands.UpdateAgenciaCommand")]
+    [InlineData("EVANS.Application.Catalogo.Commands.DeactivateAgenciaCommand")]
+    public void UnsupportedCommands_AreAbsent(string typeName)
+    {
+        typeof(CreateClienteCommand).Assembly.GetType(typeName).Should().BeNull();
+    }
+}
